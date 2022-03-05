5f6a8065a293981f777ab7006d48fbdc9a69c3eb
Brant <xkdcc@users.noreply.github.com>
2022-03-05 21:52:58 +0800

    Finish "Pass data to a parent component"

--- petstore/src/app/product-alerts/product-alerts.component.ts
+++ petstore/src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,5 @@
 // To set up ProductAlertsComponent to receive product data, first import Input from @angular/core.
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -10,7 +10,10 @@ import { Product } from '../products';
 export class ProductAlertsComponent implements OnInit {
 
   // The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent.
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+  // Configuring ProductAlertsComponent with an @Output() allows the ProductAlertsComponent to 
+  // emit an event when the value of the notify property changes.
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
