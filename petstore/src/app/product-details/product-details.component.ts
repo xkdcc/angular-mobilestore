ae980b3b905c52609f2b83583f9b4ab6e7f7a18c
Brant <xkdcc@users.noreply.github.com>
2022-03-05 22:01:52 +0800

    Finish "View product details"

--- petstore/src/app/product-details/product-details.component.ts
+++ petstore/src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+  // Inject ActivatedRoute into the constructor() by adding private route: ActivatedRoute 
+  // as an argument within the constructor's parentheses.
+  // ActivatedRoute is specific to each component that the Angular Router loads. 
+  // ActivatedRoute contains information about the route and the route's parameters.
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
