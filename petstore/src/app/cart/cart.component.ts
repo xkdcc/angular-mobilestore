53c67d7da897f9530163b2ed83bdc896350e3d1f
Brant <xkdcc@users.noreply.github.com>
2022-03-06 10:26:38 +0800

    Finish "Define the checkout form model"

--- petstore/src/app/cart/cart.component.ts
+++ petstore/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,10 +10,21 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
 }
